b1bfa28cb822de8d98fbcfaf881851e1a36ec94a
Mariusz Klinger <mariusz@mariuszklinger.pl>
2019-05-15 15:56:16 +0200

    package update

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const
     .use(body_parser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(PORT, () => console.log('webhook is listening'));
 
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
