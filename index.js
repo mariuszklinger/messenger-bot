f810c3dbe031d1dec46e567837b373e27d3e0548
Mariusz Klinger <mariusz@mariuszklinger.pl>
2019-05-15 16:26:59 +0200

    ...

--- index.js
+++ index.js
@@ -121,8 +121,13 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      "text": `Thanks for the message: {received_message.text}, prepare for surprise soon!`,
     }
+
+    setTimeout(() => callSendAPI(sender_psid, {
+      text: 'Surprise!!!1111'
+    }), 60000);
+
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
     const attachment_url = received_message.attachments[0].payload.url;
